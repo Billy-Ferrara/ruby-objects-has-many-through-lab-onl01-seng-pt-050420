7a65f31ad4d5e8e15c2ca5822c7300aa03d3325a
Billy Ferrara <willfira717@gmail.com>
2020-06-07 20:07:01 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,28 +1,49 @@
+# class Doctor
+#
+#   attr_accessor :name, :appointments, :patient
+#
+#   @@all =[]
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def initialize(name)
+#     @name = name
+#     @appointments = []
+#     @@all << self
+#   end
+#
+#
+#   def add_appointment(appointment)
+#      @appointments << appointment
+#   end
+#
+#   def patients
+#     @appointments.collect do |appointment|
+#       appointment.patient
+#     end
+#   end
+#
+# end
+
 class Doctor
-
-  attr_accessor :name, :appointments, :patient
-
-  @@all =[]
-
-  def self.all
-    @@all
-  end
+  attr_accessor :name
 
   def initialize(name)
     @name = name
     @appointments = []
-    @@all << self
   end
 
-
   def add_appointment(appointment)
-     @appointments << appointment
+    @appointments << appointment
+  end
+
+  def appointments
+    @appointments
   end
 
   def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
+    self.appointments.collect {|appointment| appointment.patient}
   end
-
 end
