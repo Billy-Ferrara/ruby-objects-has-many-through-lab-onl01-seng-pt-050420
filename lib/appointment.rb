c576184503cc97955db9fed30007cc0cdd360932
Billy Ferrara <willfira717@gmail.com>
2020-06-07 20:07:32 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,19 +1,29 @@
+# class Appointment
+#
+#   attr_accessor :patient, :doctor, :name
+#
+#
+#
+#   def initialize(name, patient)
+#     @date = date
+#     self.patient = patient
+#     patient.add_appointment(self)
+#     @doctor = doctor
+#
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+# end
+
 class Appointment
-
-  attr_accessor :patient, :doctor, :name
-
-
+  attr_accessor :doctor, :patient, :name
 
   def initialize(name, patient)
-    @date = date
+    @name = name
     self.patient = patient
     patient.add_appointment(self)
-    @doctor = doctor
-
   end
-
-  def self.all
-    @@all
-  end
-
 end
