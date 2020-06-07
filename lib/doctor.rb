cc916693b689cf209220eae9fd619cdde526972e
Billy Ferrara <willfira717@gmail.com>
2020-06-07 19:56:46 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,15 +4,16 @@ class Doctor
 
   @@all =[]
 
+  def self.all
+    @@all
+  end 
+
   def initialize(name)
     @name = name
     @appointments = []
     @@all << self
   end
 
-  def self.all
-    @@all
-  end
 
   def new_appointment(date, patient)
      appointment = Appointment.new(patient, date, self)
@@ -23,7 +24,8 @@ class Doctor
   def patients
     @appointments.collect do |appointment|
       appointment.patient
-    end
+    e
+    nd
   end
 
 
