8a87ee5836f51ba5a52bc1e6011c8eac96bd307a
Billy Ferrara <willfira717@gmail.com>
2020-06-07 20:10:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,46 +1,25 @@
-# class Patient
-#
-#   attr_reader :name
-#
-#   def initialize(name)
-#     @name = name
-#   end
-#
-#   def new_appointment(doctor, date)
-#     Appointment.new(date, self, doctor)
-#   end
-#
-#   def appointments
-#     Appointment.all.select do |appointment|
-#       appointment.patient == self
-#     end
-#   end
-#
-#   def doctors
-#     appointments.map do |appointment|
-#       appointment.doctor
-#     end
-#   end
-#
-# end
 class Patient
-  attr_accessor :name
+
+  attr_reader :name
 
   def initialize(name)
     @name = name
-    @appointments = []
   end
 
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
-    @appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
 
   def doctors
-    self.appointments.collect {|appointment| appointment.doctor}
+    appointments.map do |appointment|
+      appointment.doctor
+    end
   end
+
 end
